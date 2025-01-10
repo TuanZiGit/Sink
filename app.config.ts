0516e987ea62bcecd4e733758b24d28e85b9d2bb
团子 <46892455+TuanZiGit@users.noreply.github.com>
2025-01-10 20:45:29 +0800

    1

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,8 @@
 export default defineAppConfig({
-  title: 'Sink',
-  email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/ccbikai/sink',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  title: '团子のSink',
+  email: 'personal@lixiaotuan.cn',
+  github: 'https://github.com/TuanZiGit/sink',
+  description: '无服务器的高效短链接服务',
   image: 'https://sink.cool/banner.png',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
